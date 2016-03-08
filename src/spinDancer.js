5c87b0325aac026c24a41e883df51c12097afaec
Koala-t <johnsonc.88@gmail.com>
2016-03-08 11:12:00 -0800

    Implement SpinDancer subclass

--- src/spinDancer.js
+++ src/spinDancer.js
@@ -1,5 +1,6 @@
 var SpinDancer = function (top, left, timeBetweenSteps) {
 
+  
   Dancer.call(this, top, left, timeBetweenSteps);
 
 };
@@ -14,9 +15,25 @@ SpinDancer.prototype.step = function (timeBetweenSteps) {
 
   Dancer.prototype.step.call(this, timeBetweenSteps);
 
+  // if (this.location === 'first'){
+    // console.log(this.$node.top);
   this.$node.animate({
-    top: "+=110px",
-    left: "+=30px"
+    top:  "+=20px",
+    left: "+=40px"
   });
+    // this.location = 'second';
+  
+  // }
+
+  // if (this.location === 'second') {
+    
+  //   this.$node.animate({
+  //     top: "-=110px",
+  //     left: "-=30px"
+  //   });
+
+  //   this.location = 'first';
+  // }
+
 
 };
